d1d548656c10525bc2c74c340da17e0b3bd6be41
Yash Patil <140960782+YashPatil1402@users.noreply.github.com>
2025-07-10 17:39:43 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwD3FG3KuBLdE0vit4ltAINYrkyP95A2GbdTUOf15yDwdwi-1bGTUIhGgH5oBkCWdNvlg/exec';
+const form = document.getElementById('contact-form');
+const response = document.getElementById('response');
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+  const data = Object.fromEntries(new FormData(form).entries());
+  fetch(scriptURL, {
+    method: 'POST',
+    body: JSON.stringify(data)
+  })
+  .then(res => {
+    response.innerText = "Message sent successfully!";
+    form.reset();
+  })
+  .catch(err => {
+    response.innerText = "Something went wrong!";
+    console.error(err);
+  });
+});
